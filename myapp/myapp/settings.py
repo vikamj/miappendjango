2b48c4818eed393db1bbe67a4826d0f9c1b0c57f
victoria <vikamj@hotmail.com>
2023-01-17 11:43:45 +0100

    empezamos con css, bootstrap...

--- myapp/myapp/settings.py
+++ myapp/myapp/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-py04c#y1n3qy+ih2-4z*5o#unlxxctmm@vgzaai=8jsdm4t$lq'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
